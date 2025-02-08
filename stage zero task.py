bfbd67921612cc723280f69e00b3875ec50f112c
Adewuyi Emmanuel <emmanueladewuyi69@gmail.com>
2025-02-08 12:24:33 +0100

    Add files via upload
    this file contains the code to the  hackbio internship stage zero task. The code contains the information of each team member. The data structure type used is the tuple, i used python fiddle which is a web based app as my code editor.

---
+++ stage zero task.py
@@ -0,0 +1,53 @@
+team_memberA = "Name:Adewuyi Emmanuel", "Username:EmmanuelEA", "email:emmanueladewuyi69@gmail.com", "Hobby:photography", "country:Nigeria", "Discipline:Biochemistry", "preferred programming language:python"
+print(team_memberA[0])
+print(team_memberA[1])
+print(team_memberA[2])
+print(team_memberA[3])
+print(team_memberA[4])
+print(team_memberA[5])
+print(team_memberA[6])
+
+print()
+print("Team member 2")
+team_memberB = "Name:Adegite Adejuwon", "slack username:a_adegite", "email:adegiteadejuwon@gmail.com", "Hobby:Teaching", "country:Nigeria", "Discipline:Cell Biology and Genetics", "Preferred programming language:Python"
+print(team_memberB[0])
+print(team_memberB[1])
+print(team_memberB[2])
+print(team_memberB[3])
+print(team_memberB[4])
+print(team_memberB[5])
+print(team_memberB[6])
+
+print()
+print("Team member 3")
+
+team_memberC = "Name:Mgbafulu Chioma", "Slack username:Chioma", "Email:chiomannamdi23@gmail.com", "Hobby:listening to music", "Country:Nigeria", "Discipline:Pharmacy", "Preferred programming language: Python"
+print(team_memberC[0])
+print(team_memberC[1])
+print(team_memberC[2])
+print(team_memberC[3])
+print(team_memberC[4])
+print(team_memberC[5])
+print(team_memberC[6])
+
+print()
+print("Team member 4")
+team_memberD = "Name:Ajudua Sharon", "Slack username:SharonAjudua", "email:sharonajudua@gmail.com","Hobby:Trying out new recipes,Travelling","Country:Nigeria", "Discipline:Medicine", "Preferred programming language:Python"
+print(team_memberD[0])
+print(team_memberD[1])
+print(team_memberD[2])
+print(team_memberD[3])
+print(team_memberD[4])
+print(team_memberD[5])
+print(team_memberD[6])
+
+print()
+print("Team member 5")
+team_memberE = "Name:Maurice Godwin", "Slack username:maurice", "Email:mauricemary74@gmail.com", "Hobby:Reading", "Country:Nigeria", "Discipline:Biotechnology", "Preferred programming language:Python"
+print(team_memberE[0])
+print(team_memberE[1])
+print(team_memberE[2])
+print(team_memberE[3])
+print(team_memberE[4])
+print(team_memberE[5])
+print(team_memberE[6])
